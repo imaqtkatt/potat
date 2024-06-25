403c7d8151d5505241f6495b0b2c921807d52ba0
imaqtkatt <eduarda.so@proton.me>
2024-06-24 22:43:34 -0300

    small updates, read packet data

--- script.py
+++ script.py
@@ -3,16 +3,19 @@
 import socket
 
 HOST = '127.0.0.1'
-PORT = 9000
+PORT = 3000
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((HOST, PORT))
 
-bytes = bytearray()
-bytes.append(3)
-bytes.append(0)
-bytes.append(1)
+b = bytearray()
+b.append(0)
+b.append(0)
+b.append(3)
+b.append(42)
+b.append(42)
+b.append(42)
 
-sock.sendall(bytes)
+sock.sendall(b)
 
 exit(0)
--- src/main.zig
+++ src/main.zig
@@ -1,37 +1,63 @@
 const std = @import("std");
 const net = std.net;
 
-const packet = packed struct {
+var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+
+const Packet = struct {
     tag: u8,
     length: u16,
+    data: []u8,
+
+    const Self = @This();
+
+    fn destroy(self: *Self) void {
+        gpa.allocator().free(self.data);
+    }
+
+    fn print_data(self: *Self) void {
+        std.debug.print("{s}\n", .{self.data});
+    }
 };
 
-pub fn read_packet(stream: net.Stream) !packet {
+pub fn read_packet(stream: net.Stream) net.Stream.ReadError!Packet {
     var buf: [3]u8 = undefined;
-    std.debug.assert(try stream.read(&buf) == 3);
+    const len = try stream.readAtLeast(&buf, 3);
 
-    return packet{
+    if (len < 3) return net.Stream.ReadError.Unexpected;
+
+    const buf_data = gpa.allocator().alloc(u8, len) catch {
+        return net.Stream.ReadError.SystemResources;
+    };
+    const data = try stream.readAtLeast(buf_data, len);
+
+    if (data < len) return net.Stream.ReadError.Unexpected;
+
+    return Packet{
         .tag = buf[0],
         .length = @as(u16, buf[1]) << @as(u16, 8) | @as(u16, buf[2]),
+        .data = buf_data,
     };
 }
 
-pub fn main() !void {
-    var running = true;
-    var server = net.StreamServer.init(.{});
-    defer {
-        server.deinit();
-        server.close();
-    }
+const addr = net.Address.initIp4(.{ 127, 0, 0, 1 }, 3000);
 
-    try server.listen(try net.Address.parseIp("127.0.0.1", 9000));
+pub fn main() !void {
+    const opts = net.Address.ListenOptions{ .reuse_address = true, .force_nonblocking = false, .kernel_backlog = 128, .reuse_port = true };
+    var server = try net.Address.listen(addr, opts);
+    defer server.deinit();
+
     std.log.info("Listening at: {}\n", .{server.listen_address});
 
-    while (running) {
+    while (true) {
         var conn = try server.accept();
         defer conn.stream.close();
 
-        var pkt = try read_packet(conn.stream);
-        std.log.info("Packet = {}", .{pkt});
+        var pkt = read_packet(conn.stream) catch |e| {
+            std.log.err("{}", .{e});
+            continue;
+        };
+        defer pkt.destroy();
+        std.log.info("pkt = {}", .{pkt});
+        pkt.print_data();
     }
 }
